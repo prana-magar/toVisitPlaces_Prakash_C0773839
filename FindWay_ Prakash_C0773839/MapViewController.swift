03149395c51834a296d081240d2ad011f3143140
prana <prakashrana9847067614@gmail.com>
2020-06-13 13:03:41 -0400

    Added permission for location and mapkiit

--- FindWay_ Prakash_C0773839/MapViewController.swift
+++ FindWay_ Prakash_C0773839/MapViewController.swift
@@ -7,14 +7,24 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+}
+
+
+
+// extension to add map functionalities
+extension MapViewController{
+    
+    
 }
 
