91b7521f14efadd6db1b198fc1c6de8202a9b25a
prana <prakashrana9847067614@gmail.com>
2020-06-13 12:49:56 -0400

    Initial Commit

---
+++ FindWay_ Prakash_C0773839/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FindWay_ Prakash_C0773839
+//
+//  Created by Prakash on 2020-06-13.
+//  Copyright © 2020 com.lambton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
